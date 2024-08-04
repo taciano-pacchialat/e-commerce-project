1caa0ecf8e5718d04201eaf39203b71507d8dfe5
Taciano <taciano3003@gmail.com>
2024-08-03 21:46:28 -0300

    product detail view, responsive

--- frontend/lib/views/store/product_detail_view.dart
+++ frontend/lib/views/store/product_detail_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/services/store/product.dart';
 
@@ -8,54 +9,71 @@ class ProductDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: product.images.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(product.images[index]),
-                  );
-                },
-              ),
-            ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.only(
+                left: screenWidth * 0.08,
+                right: screenWidth * 0.08,
+                top: screenHeight * 0.01,
+              ),
               child: Text(
                 product.title,
                 style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: screenHeight * 0.7,
+                viewportFraction: 0.98,
+                enlargeCenterPage: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
+                enableInfiniteScroll: false,
+              ),
+              items: product.images.map(
+                (image) {
+                  return Builder(
+                    builder: (context) {
+                      return Image.asset(image, fit: BoxFit.contain);
+                    },
+                  );
+                },
+              ).toList(),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 0.0,
+                horizontal: screenWidth * 0.08,
+              ),
+              child: Text(
+                '\$${product.unitPrice.toStringAsFixed(2)}',
+                style: const TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.normal,
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.only(
+                top: screenWidth * 0.02,
+                bottom: 0.0,
+                left: screenWidth * 0.05,
+                right: screenWidth * 0.05,
+              ),
               child: Text(
                 product.description,
                 style: const TextStyle(
                   fontSize: 16,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                '\$${product.unitPrice.toStringAsFixed(2)}',
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
                 ),
               ),
             ),
