0ea2a7c815dd0472dd2afe90d75777d8c6a65dce
Taciano <taciano3003@gmail.com>
2024-09-26 21:54:56 -0300

    id field to category

--- lib/services/store/category.dart
+++ lib/services/store/category.dart
@@ -1,9 +1,11 @@
 class Category {
+  final String id;
   final String name;
   final int items;
   final List<Category> subCategories;
 
   Category({
+    required this.id,
     required this.name,
     required this.items,
     this.subCategories = const [],
